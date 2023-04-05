f1215fd93cd7ca9440a39a98ed822a44f8d8f563
MikhailBezhaneishvili <mike215misha@gmail.com>
2023-04-05 08:45:04 +0300

    Cli.java исправил чекстайл

--- App/src/main/java/hexlet/code/Cli.java
+++ App/src/main/java/hexlet/code/Cli.java
@@ -3,8 +3,8 @@ package hexlet.code;
 import java.util.Scanner;
 
 public  class Cli {
-    public static String name(){
-Scanner scanner = new Scanner(System.in);
+    public static String name() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("What is your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
